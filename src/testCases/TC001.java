455b89335b2902976dcbe4e065febd857f491155
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-05 14:14:59 +0200

    TC001 updated

--- src/testCases/TC001.java
+++ src/testCases/TC001.java
@@ -2,8 +2,9 @@ package testCases;
 
 public class TC001 {
     public static void main(String[] args) {
-        //testing some thing
+        //testing some things
 
         //more testing
+
     }
 }
